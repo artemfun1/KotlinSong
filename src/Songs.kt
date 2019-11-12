6d2a6086c2953d25f10091f2dcee02bc36fb46b8
Artemfun <gitartem1@yandex.ru>
2019-11-13 00:30:04 +0300

    end

---
+++ src/Songs.kt
@@ -0,0 +1,18 @@
+
+
+fun main(){
+    val songOne = Song("The Mesopotamians", "They Might Be Giants")
+    val songTwo = Song("Going Underground", "The Jam")
+    val songThree = Song("Make Me Smile", "Steve Harley")
+    songTwo.play()
+    songTwo.stop()
+    songThree.play()
+}
+class Song (val title:String, val artist:String){
+    fun play(){
+        println("Playing the song $title by $artist")
+    }
+    fun stop(){
+        println("Stopped playing $title")
+    }
+}
